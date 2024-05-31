bf4ef3a6e2bee1d1c33c2e15df93a18878f539cc
LIGENGCI <s1040704@mqjh.tw>
2024-05-31 16:50:55 +0800

    112/5/31

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,10 @@
+/** 
+ * @author 李耕棋 <s1040704@mqjh.tw>
+ */
+export default function CustomCard({item}){
+    return(
+        <div class = "border">
+              <div>{item.ScenicSpotName}</div>  
+        </div>
+    )
+}
